611670ef35ef746e9b07a6d8d80a8b6fde4f60c9
iamnguyenvu <iamnguyenvu.gm@gmail.com>
2024-04-20 18:07:49 +0700

    update cart

--- js/script.js
+++ js/script.js
@@ -142,6 +142,11 @@ $(document).ready(function() {
     });
   }
 
+  let favourite = JSON.parse(localStorage.getItem("favourite")) || [];
+  let carts = JSON.parse(localStorage.getItem("carts")) || [];
+  $("#favourite").text(favourite.length);
+  $("#cart").text(carts.length);
+
 });
 
 

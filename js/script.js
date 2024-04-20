2da36bfbfbbebf2f85b3770a61f994a8f9c2051f
iamnguyenvu <iamnguyenvu.gm@gmail.com>
2024-04-21 00:00:09 +0700

    filter by category

--- js/script.js
+++ js/script.js
@@ -147,6 +147,93 @@ $(document).ready(function() {
   $("#favourite").text(favourite.length);
   $("#cart").text(carts.length);
 
+  function sortByPrice(order) {
+    products.sort(function(a, b) {
+      if (order === 'higher') {
+        return b.price - a.price;
+      } else if (order === 'lower') {
+        return a.price - b.price;
+      }
+    });
+    displayProductsOnPage(currentPage);
+  }
+  
+  $("#slSort").change(function() {
+    let selectedOption = $(this).val();
+    sortByPrice(selectedOption);
+  });
+
+  function filterProductsByCategory() {
+    let selectedCategories = [];
+    $("input[type='checkbox']:checked").each(function() {
+      selectedCategories.push($(this).attr("name"));
+    });
+  
+    if (selectedCategories.length === 0) {
+      displayProductsOnPage(currentPage);
+    } 
+    else {
+      let filteredProducts = products.filter(function(product) {
+        return selectedCategories.includes(product.category);
+      }); 
+      displayFilteredProducts(filteredProducts);
+    }
+    
+  }
+
+  function displayFilteredProducts(products) {
+    $("#home-products").empty();
+    products.forEach(function(product) {
+      let col = document.createElement("div");
+    col.className = "col-3";
+
+    let card = document.createElement("div");
+    card.className = "card";
+
+    let cardBody = document.createElement("a");
+    cardBody.className = "card-body";
+    cardBody.id = product.id;
+    cardBody.href = "../html/mu-bao-hiem.html";
+
+    let img = document.createElement("img");
+    img.src = product.img;
+    img.className = "card-img";
+
+    let title = document.createElement("h6");
+    title.className = "card-title title-center form-control mb-3 pb-3 font-weight-bold";
+    title.textContent = product.title;
+    title.style.border = "unset"; 
+    title.style.fontSize = "14px";
+    title.style.height = "3rem";
+
+    let price = document.createElement("span");
+    price.className = "card-price mt-3 mb-3";
+    price.textContent = "Giá: " + product.price + " VND";
+    price.style.color = "black";
+
+    let button = document.createElement("button");
+    button.className = "card-btn form-control";
+    button.style.background = "#ff6319";
+    button.textContent = "Mua ngay";
+    button.style.color = "white"; 
+
+    cardBody.appendChild(img);
+    cardBody.appendChild(title);
+    cardBody.appendChild(price);
+    cardBody.appendChild(button);
+
+    card.appendChild(cardBody);
+    
+    col.appendChild(card);
+
+    $("#home-products").append(col);
+    });
+  }
+  $("input[type='checkbox']").change(function() {
+    filterProductsByCategory();
+  });
+
+
 });
 
 
